4318a2cb6a13c58487f9577659b9483415477420
Irina Dobrenco <irika.22181@gmail.com>
2024-11-17 13:46:32 +0200

    first commit

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,8 +3,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/app.ts',
     mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'dist'),
